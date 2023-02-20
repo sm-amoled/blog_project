6b205e2652603baca405b226617c272ffaf52dab
sm-amoled <moktak072@naver.com>
2023-02-20 21:37:42 +0900

    feat - example project 삭제

--- nextjs-notion-starter-kit-main/site.config.ts
+++ nextjs-notion-starter-kit-main/site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'd60975c3ba0d4bee8075d605c0ff59cc',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '박성민 Notion 블로그',
+  domain: 'www.led-dev.xyz',
+  author: '박성민',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
